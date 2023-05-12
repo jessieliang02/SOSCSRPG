032e7697c5fe0949154a548adec0d194465d2706
Jessie Liang <jessieliang2015ca@gmail.com>
2023-05-11 17:42:51 -0700

    added monsters to ui

--- Engine/ViewModels/GameSession.cs
+++ Engine/ViewModels/GameSession.cs
@@ -12,6 +12,7 @@ namespace Engine.ViewModels
     public class GameSession : BaseNotificationClass
     {
         private Location _currentLocation;
+        private Monster _currentMonster;
         public Player CurrentPlayer { get; set; }
         public Location CurrentLocation 
         { 
@@ -28,9 +29,24 @@ namespace Engine.ViewModels
                 OnPropertyChanged(nameof(HasLocationToEast));
 
                 GivePlayerQuestsAtLocation();
+                GetMonsterAtLocation();
             } 
         }
         public World CurrentWorld { get; set; }
+
+        public Monster CurrentMonster 
+        { 
+            get { return _currentMonster; } 
+            set
+            {
+                _currentMonster = value;
+
+                OnPropertyChanged(nameof(CurrentMonster));
+                OnPropertyChanged(nameof(HasMonster));
+            }
+        }
+
+        public bool HasMonster => CurrentMonster != null;
 
         public bool HasLocationToNorth
         {
@@ -116,6 +132,11 @@ namespace Engine.ViewModels
                     CurrentPlayer.Quests.Add(new QuestStatus(quest));
                 }
             }
+        }
+
+        private void GetMonsterAtLocation()
+        {
+            CurrentMonster = CurrentLocation.GetMonster();
         }
     }
 }
